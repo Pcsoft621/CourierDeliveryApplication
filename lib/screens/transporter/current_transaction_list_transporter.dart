e56c53117b5e46a3ce6ba7a7423411bf85b54dfa
Pcsoft <pcsoft621@gmail.com>
2022-11-29 00:02:25 +0530

    before Admin panel

--- lib/screens/transporter/current_transaction_list_transporter.dart
+++ lib/screens/transporter/current_transaction_list_transporter.dart
@@ -1,23 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_go/screens/consumer/specific_courier_details_scren.dart';
+import 'package:easy_go/screens/transporter/track_transporter_curr_trans.dart';
 import 'package:easy_go/util/firebase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:easy_go/screens/consumer/specific_courier_details_scren.dart';
 
 class CurrentTransactionsForTr extends StatefulWidget {
-  const CurrentTransactionsForTr({super.key});
+  const CurrentTransactionsForTr({
+    super.key,
+  });
 
   @override
   State<CurrentTransactionsForTr> createState() =>
       _CurrentTransactionsForTrState();
 }
 
-// claass chya outside variable declare kartyat ka
-//karn mg tith initialize nahi hot jr aat kel tr
 class _CurrentTransactionsForTrState extends State<CurrentTransactionsForTr> {
   final storage = FirebaseStorage.instance;
-  final uid = FirebaseAuth.instance.currentUser!.uid;
+  
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    
   String? from, to;
   Map<String, dynamic>? user;
   List<Map<String, dynamic>>? couriers;
@@ -53,31 +56,60 @@ class _CurrentTransactionsForTrState extends State<CurrentTransactionsForTr> {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Text("Loading");
                   }
-                  
+
                   return ListView(
+                    
                     children:
                         snapshot.data!.docs.map((DocumentSnapshot document) {
                       Map<String, dynamic> data =
                           document.data()! as Map<String, dynamic>;
+                      data["id"] = document.id;
+                      return Card(
                       
-                      return ListTile(
+                        /*decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const[
+                            BoxShadow(
+                            color: Color.fromARGB(0, 32, 31, 31),
+                            spreadRadius: -4,
+                            blurRadius: 25,
+                            offset: Offset(0, 4),
+                          )
+                          ]
+                        ),*/
+                        child: ListTile(
+                        tileColor: Color.fromARGB(137, 220, 61, 133),
+                        horizontalTitleGap: 10,
                         leading: CircleAvatar(
                           radius: 30.0,
+
                           backgroundImage: NetworkImage(data[
                                   "courierImageUrl"] ??
                               "https://ptyagicodecamp.github.io/images/profile.jpg"),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        title: Text(data['courierName'] +
-                            " " +
-                            data['locationFrom'] +
-                            " " +
-                            data['locationTo']),
-                       
-                        onTap: () {
-                           
+                          backgroundColor: Colors.black,
                           
+                        ),
+                        title: Text(data['courierName'] 
+                                ),
+                        subtitle: Text(''+data['locationFrom'] +' to '+data['locationTo']),
+                        trailing: Icon(Icons.more_vert),
+                        onTap: () {
+                         
+                          Navigator.push(
+                            //pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  SpecificCourierDetails(
+                                courierId: data["id"] as String,
+                                senderId: data["courierSenderId"],
+                                courierTitle:
+                                    "${data['courierName']} ${data['locationFrom']} ${data['locationTo']}",
+                              ),
+                            ),
+                          );
                         },
+                      ),
                       );
                     }).toList(),
                   );
